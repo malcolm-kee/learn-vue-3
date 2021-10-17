31e6b3979bd23ef5f6ae57cb02afb255d7fbb316
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-10-17 10:27:40 +0800

    day 1

---
+++ src/components/icon-button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+    title: string;
+}>();
+
+const emit = defineEmits(['click'])
+</script>
+
+<template>
+    <button
+        type="button"
+        @click="emit('click')"
+        class="p-1 rounded-full hover:bg-gray-50 focus:outline-none focus:bg-gray-50 focus:ring focus:ring-pink-500 focus:ring-opacity-30 transition duration-150 ease-in-out"
+        :title="props.title"
+    >
+        <slot></slot>
+    </button>
+</template>
