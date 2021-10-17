4b999e112fe32f9944cf1a89188da87e24b6b2d9
malcolm-kee <malcolm.keeweesiong@gmail.com>
2021-10-17 08:42:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-tnfyf2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-tnfyf2)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import CareerItem from './components/career-item.vue';
+
+function onEdit(title) {
+  console.log('handle edit in app', title);
+}
+</script>
+
+<template>
+  <ul class="md:flex-1 space-y-3">
+    <li>
+      <CareerItem title="Memer" department="UI / UX Design" @edit="onEdit" />
+    </li>
+    <li>
+      <CareerItem
+        title="Slack Chatter"
+        department="Engineering"
+        @edit="onEdit"
+      />
+    </li>
+    <li>
+      <CareerItem title="Janitor" department="Health/Welfare" @edit="onEdit" />
+    </li>
+  </ul>
+</template>
+
+<style></style>
